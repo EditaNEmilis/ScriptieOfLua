6ee40db01ca491c65ba1d25945a3b740a62dd9d9
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-02-03 19:16:57 +0200

    DropToolsOnDeath update
    Code messages and rootPart position for the tools

--- Roblox/LocalScript/StarterPlayer/StarterCharacterScripts/Tools/DropToolsOnDeath.lua
+++ Roblox/LocalScript/StarterPlayer/StarterCharacterScripts/Tools/DropToolsOnDeath.lua
@@ -1,16 +1,22 @@
+-- Locals
 local character = script.Parent
 local players = game.Players
 
+-- If the player dies
 character.Humanoid.Died:Connect(function()
 	for _, tool in pairs(character:GetChildren()) do
 		if (tool.ClassName == "Tool") then
-			tool.Parent = game.Workspace
+			tool.Parent = game.Workspace -- Remove it from the player
 		end
 	end
-	
+
 	if players[character.Name]:FindFirstChild("Backpack") then
 		for _, tool in pairs(players[character.Name].Backpack:GetChildren()) do
-			tool.Parent = game.Workspace
+			tool.Parent = game.Workspace -- Remove it from the backpack
+
+			-- RootPart
+			local rootPart = character:FindFirstChild("HumanoidRootPart")
+			tool:FindFirstChild("Handle").Position = rootPart.Position -- Set the tool(s) to the RootPart position
 		end
 	end
 end)
