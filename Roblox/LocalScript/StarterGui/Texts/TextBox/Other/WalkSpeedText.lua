aa32c6e068c15646621cce35d74e167fc6a56003
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-03-14 19:02:13 +0200

    TextBox update
    Works the same as TextLabel before.

---
+++ Roblox/LocalScript/StarterGui/Texts/TextBox/Other/WalkSpeedText.lua
@@ -0,0 +1,7 @@
+local textbox = script.Parent.TextBox
+local players = game.Players.LocalPlayer
+
+local char = players.Character or players.CharacterAdded:Wait()
+local humanoid = char:WaitForChild("Humanoid")
+
+textbox.Text = humanoid.WalkSpeed
