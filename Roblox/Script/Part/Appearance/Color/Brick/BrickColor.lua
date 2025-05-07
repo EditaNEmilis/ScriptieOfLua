8748b5f4ce646c5832e78d1ee5a83f5d32b4f9b5
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-05-07 21:05:33 +0300

    Minor update
    Some minor tweaks and code messages for some scripts.

--- Roblox/Script/Part/Appearance/Color/Brick/BrickColor.lua
+++ Roblox/Script/Part/Appearance/Color/Brick/BrickColor.lua
@@ -2,10 +2,11 @@ local brick = script.Parent
 
 -- Repeat Loop
 while true do
-	brick.BrickColor = BrickColor.new("Really red")
+	brick.BrickColor = BrickColor.new("Really red") -- Red
 	wait(1)
-	brick.BrickColor = BrickColor.new("Really blue")
+	brick.BrickColor = BrickColor.new("Really blue") -- Blue
 	wait(1)
-	brick.BrickColor = BrickColor.new("Sea green")
+	brick.BrickColor = BrickColor.new("Sea green") -- Green
 	wait(1)
+	-- So on..
 end
--- Roblox/Script/Part/Appearance/Other/Transparency/TransparencyMeter.lua
+++ Roblox/Script/Part/Appearance/Other/Transparency/TransparencyMeter.lua
@@ -4,7 +4,7 @@ local x = 0
 while wait(.01) do
 	brick.Transparency = x
 	x = x + 0.01
-	
+
 	if x >= 1 then
 		x = 0
 	end
