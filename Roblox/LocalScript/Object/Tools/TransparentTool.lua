60cef04ab218dcf0aab450d8d70b385c2e53e078
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-06-18 21:28:32 +0300

    Transparent Tool script
    A new localscript folder section.. For tools. Yea, seriously.

---
+++ Roblox/LocalScript/Object/Tools/TransparentTool.lua
@@ -0,0 +1,29 @@
+local tool = script.Parent
+local handle = tool:WaitForChild("Handle")
+
+-- Check if the tool is being held
+tool.Equipped:Connect(function()
+	local character = tool.Parent
+	local player = game.Players:GetPlayerFromCharacter(character)
+
+	if player and player.Character == character then
+		-- Function
+		function Transparent()
+			for i = 1, 10 do
+				handle.Transparency = handle.Transparency + 0.1
+				wait(0.1)
+			end
+
+			wait(0.33) -- Delay
+
+			for i = 1, 10 do
+				handle.Transparency = handle.Transparency - 0.1
+				wait(0.1)
+			end
+		end
+	end
+end)
+
+while task.wait(0) do
+	Transparent()
+end
