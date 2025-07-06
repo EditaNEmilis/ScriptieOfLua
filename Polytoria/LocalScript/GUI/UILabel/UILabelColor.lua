d02ad15881d1d5df7c757f32265d58465f8a9227
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-07-06 18:35:50 +0300

    UILabel scripts
    UI labels with text.

---
+++ Polytoria/LocalScript/GUI/UILabel/UILabelColor.lua
@@ -0,0 +1,4 @@
+local gui = game["PlayerGUI"]["GUI"]
+local label = gui["UILabel"]
+
+label.TextColor = Color.New(0.5, 0.7, 0.33, 1) -- Uses floats instead of 0-255
---
+++ Polytoria/LocalScript/GUI/UILabel/UILabelText.lua
@@ -0,0 +1,4 @@
+local gui = game["PlayerGUI"]["GUI"]
+local label = gui["UILabel"]
+
+label.Text = "Hello World"
