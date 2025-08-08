9cbc31deba5a02ff7fa3afdf7fecd9941ea4155c
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-08-08 20:48:53 +0300

    EmitSound script
    Both for server and client.

---
+++ GarrysMod/LocalScript/Globals/Other/EmitSound.lua
@@ -0,0 +1,12 @@
+EmitSound(
+    "garrysmod/save_load1.wav", -- soundName
+    Entity(1):GetPos(), -- Position
+    1, -- Entity Index
+    CHAN_AUTO, -- Channel
+    1, -- Volume
+    75, -- SND Level
+    0, -- soundFlags
+    100, -- Pitch
+    0, -- DSP Preset
+    nil -- Filter
+)
