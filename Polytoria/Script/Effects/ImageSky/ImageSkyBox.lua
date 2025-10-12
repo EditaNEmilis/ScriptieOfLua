4b7bbed1d79dad64c97c756f8ce0bb1370d3d308
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-10-12 21:18:38 +0300

    ImageSky Box script
    A script for changing the skybox with IDs.

---
+++ Polytoria/Script/Effects/ImageSky/ImageSkyBox.lua
@@ -0,0 +1,10 @@
+local environment = game['Environment']
+local imageSky = environment['ImageSky']
+
+-- Skybox IDs
+imageSky.TopId = 38726
+imageSky.BottomId = 38732
+imageSky.LeftId = 38729
+imageSky.RightId = 38727
+imageSky.FrontId = 38728
+imageSky.BackId = 38731
