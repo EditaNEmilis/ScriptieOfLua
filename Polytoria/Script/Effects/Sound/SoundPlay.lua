4c28260d59df096c4765f331fbccae70d1248a2c
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-10-25 17:30:14 +0300

    Mini update
    Some code messages added and other.

--- Polytoria/Script/Effects/Sound/SoundPlay.lua
+++ Polytoria/Script/Effects/Sound/SoundPlay.lua
@@ -1,5 +1,6 @@
 local sound = game["Environment"]["Sound"]
 
+-- If the sound is loaded in
 sound.Loaded:Connect(function()
     sound.Play()
 end)
--- Polytoria/Script/World/NPC/NPCDied.lua
+++ Polytoria/Script/World/NPC/NPCDied.lua
@@ -1,5 +1,6 @@
 local npc = script.Parent
 
+-- NPC function
 npc.Died:Connect(function()
     print("NPC died!")
 end)
