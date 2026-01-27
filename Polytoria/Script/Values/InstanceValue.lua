d7fe99f736342c6e409b2e76c121f4894a72000b
EditaNEmilis <ramanauskiene.edita@gmail.com>
2026-01-27 19:55:09 +0200

    Instance Value
    Instance value, prints out the proxy part.

---
+++ Polytoria/Script/Values/InstanceValue.lua
@@ -0,0 +1,6 @@
+local environment = game["Environment"]
+local instanceValue = environment["InstanceValue"]
+
+instanceValue.Value = game["Environment"]["Part"] -- Instance
+
+print(instanceValue.Value)
