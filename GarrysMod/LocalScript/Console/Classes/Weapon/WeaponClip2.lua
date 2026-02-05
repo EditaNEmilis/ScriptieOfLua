bea80b4c465bc9b28dd4c93188c57c8e13f6ed60
EditaNEmilis <ramanauskiene.edita@gmail.com>
2026-02-05 19:53:42 +0200

    Weapon scripts
    Script for console, printing clip primary and secondary.

---
+++ GarrysMod/LocalScript/Console/Classes/Weapon/WeaponClip2.lua
@@ -0,0 +1,3 @@
+local entity = Entity(1)
+
+print(entity:GetActiveWeapon():Clip2()) -- Prints the amount of secondary ammo in the magazine of the weapon
---
+++ GarrysMod/Script/Console/Classes/Weapon/WeaponClip1.lua
@@ -0,0 +1,3 @@
+local entity = Entity(1)
+
+print(entity:GetActiveWeapon():Clip1()) -- Prints the amount of primary ammo in the magazine of the weapon
