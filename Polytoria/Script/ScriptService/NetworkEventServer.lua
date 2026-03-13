89ffb2b0ff1665b891086dd986796cb825a44693
EditaNEmilis <ramanauskiene.edita@gmail.com>
2026-03-13 17:08:32 +0200

    Update
    Code messages and other

--- Polytoria/Script/ScriptService/NetworkEventServer.lua
+++ Polytoria/Script/ScriptService/NetworkEventServer.lua
@@ -1,4 +1,4 @@
-local message = NetMessage.New()
-message.AddString("key", "value")
+local message = NetMessage.New() -- NetMessage
+message.AddString("key", "value") -- Add string to the NetMessage
 
 netEvent.InvokeServer(message) -- Sends a network event to the server from the client
--- Polytoria/Script/ScriptService/PlayerAward.lua
+++ Polytoria/Script/ScriptService/PlayerAward.lua
@@ -4,7 +4,7 @@ game["Players"].PlayerAdded:Connect(function(plr)
     -- Award achievement system
     Achievements:Award(plr.UserID, 12345, function(success, error)
         if success then -- If the achievement is successful
-            print("Awarded achievement")
+            print("Awarded achievement!")
         else -- Error if not succeed
             print("Error awarding achievement: " .. error)
         end
