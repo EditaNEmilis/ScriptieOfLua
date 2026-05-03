4de805c482828228e34bacb1ff48a5a44adfc52f
EditaNEmilis <ramanauskiene.edita@gmail.com>
2026-05-03 16:54:20 +0300

    Update

--- GarrysMod/Script/Libraries/string/stringJavascriptSafe.lua
+++ GarrysMod/Script/Libraries/string/stringJavascriptSafe.lua
@@ -4,4 +4,4 @@ local Panel = vgui.Create("DHTML")
 Panel:SetURL("example.com/something.html") -- URL
 Panel:Center()
 Panel:SetSize(100, 100)
-Panel:RunJavascript("MyJavaScriptFunction('" .. string.JavascriptSafe(user_input) .. "')") -- Run JavascriptSafe
+Panel:RunJavascript("MyJavaScriptFunction('" .. string.JavascriptSafe(user_input) .. "')") -- Run Javascript
